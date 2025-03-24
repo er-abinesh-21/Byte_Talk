a1f51bd1b7ad6e14e3b9e074df3db23e5f80297d
XxEr_Abinesh_MrxX <147540638+er-abinesh-21@users.noreply.github.com>
2025-03-24 13:04:57 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,12 +14,7 @@ const io = new Server(httpServer, {
 });
 
 // Middleware
-app.use(cors(
-  {
-  origin: '*',
-  methods: ['GET', 'POST'],
-}
-));
+app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
 // Track active users and their sockets
