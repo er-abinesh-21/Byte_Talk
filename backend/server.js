81e574093c43aca32b4b906ae56ab8edc20d6597
XxEr_Abinesh_MrxX <147540638+er-abinesh-21@users.noreply.github.com>
2025-03-24 12:03:26 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: 'https://byte-talk-8wwm.onrender.com',
+    origin: '*',
     methods: ['GET', 'POST'],
     credentials: true,
   },
